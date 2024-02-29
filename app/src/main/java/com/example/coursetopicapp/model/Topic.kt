50faa769ed005111895992e1ec7ef45a3be4bcf1
jcisking <gocats098@gmail.com>
2024-02-28 22:21:49 -0700

    initial commit and added Topic data class

---
+++ app/src/main/java/com/example/coursetopicapp/model/Topic.kt
@@ -0,0 +1,10 @@
+package com.example.coursetopicapp.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Topic(
+    @DrawableRes val topicImage: Int,
+    @StringRes val topicName: Int,
+    @StringRes val numberOfCourses: Int
+    )
