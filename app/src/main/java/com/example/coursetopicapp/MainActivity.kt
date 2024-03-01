cee0a3fc7120e16197d1c333af2d5f427c9d4686
jcisking <gocats098@gmail.com>
2024-03-01 16:01:41 -0700

    added TopicGrid

--- app/src/main/java/com/example/coursetopicapp/MainActivity.kt
+++ app/src/main/java/com/example/coursetopicapp/MainActivity.kt
@@ -6,10 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -23,6 +27,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.coursetopicapp.data.DataSource
+import com.example.coursetopicapp.model.Topic
 import com.example.coursetopicapp.ui.theme.CourseTopicAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,23 +41,47 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    CourseTopicApp()
                 }
             }
         }
     }
 }
+@Composable
+fun CourseTopicApp() {
+    TopicGrid()
+}
+
+@Composable
+fun TopicGrid(
+    modifier: Modifier = Modifier
+) {
+    LazyVerticalGrid(
+        modifier = modifier,
+        columns = GridCells.Fixed(2) ,
+    ) {
+        items(DataSource().returnListOfTopics()) { topic ->
+            TopicCard(
+                topic = topic,
+                modifier = Modifier.padding(8.dp)
+            )
+            
+        }
+        
+    }
+}
 
 @Composable
 fun TopicCard(
+    topic: Topic,
     modifier: Modifier = Modifier
 ) {
     Card(
-        modifier = modifier.padding(30.dp)
+        modifier = modifier
     ) {
         Row {
             Image(
-                painter = painterResource(id = R.drawable.fashion),
+                painter = painterResource(id = topic.topicImage),
                 contentScale = ContentScale.FillHeight,
                 contentDescription = null
             )
@@ -60,7 +90,7 @@ fun TopicCard(
                     .padding(start = 12.dp, end = 12.dp, top = 12.dp)
             ) {
                 Text(
-                    text = stringResource(id = R.string.fashion),
+                    text = stringResource(id = topic.topicName),
                     modifier = Modifier
                 )
                 Row {
@@ -69,7 +99,7 @@ fun TopicCard(
                         contentDescription = null
                     )
                     Text(
-                        text = "456"
+                        text = topic.numberOfCourses.toString()
                     )
                 }
             }
@@ -84,7 +114,7 @@ fun TopicCard(
 @Composable
 fun TopicCardPreview() {
     CourseTopicAppTheme {
-        TopicCard()
+//        TopicCard()
 
     }
 }
