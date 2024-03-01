63aa541d1dbf6ec250e280db3f485fde88e3aafa
jcisking <gocats098@gmail.com>
2024-02-29 19:21:12 -0700

    added TopicCard and icon

--- app/src/main/java/com/example/coursetopicapp/MainActivity.kt
+++ app/src/main/java/com/example/coursetopicapp/MainActivity.kt
@@ -3,12 +3,19 @@ package com.example.coursetopicapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.coursetopicapp.ui.theme.CourseTopicAppTheme
 
@@ -29,14 +36,42 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun TopicCard(
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+    ) {
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.fashion),
+                contentDescription = null
+            )
+            Column {
+                Text(text = stringResource(id = R.string.fashion) )
+                Row {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_grain),
+                        contentDescription = null
+                    )
+                    Text(
+                        text = "456"
+                    )
+                }
+            }
+        }
+    }
 
+}
 
 
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TopicCardPreview() {
     CourseTopicAppTheme {
+        TopicCard()
 
     }
 }
